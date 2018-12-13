eacc1639f8c56cfc9f7b9d0522162e4319c78803
ruthnewman <ruthnewman@gmail.com>
2018-12-13 09:20:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School 
+  
+  attr_accessor :roster, :name, :student, :grade
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def roster
+    @roster = {}
+  end 
+  
+  def roster 
+  
+  def add_student(student, grade)
+    roster[grade] = []
+    self.roster[grade] << student
+  end 
+  
+  def grade(grade)
+    self.roster[grade]
+  end 
+  
+  def sort 
+    
+  end 
+  
+end 
